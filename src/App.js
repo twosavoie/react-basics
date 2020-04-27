79ddae4c9bf0021f92c9b8111f1e10ffa38873f1
lsavoie <twosavoie@gmail.com>
2020-04-27 13:23:47 -0500

    first commit, made notes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import{BrowserRouter, Switch, Route} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/">
+        Home //can have different components creating different pages
+      </Route>
+      <Route path="/about">
+        About Us
+      </Route>
+
+    </Switch>
+  </BrowserRouter>
+  </>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//const element = <><h1>Hello world!</h1><p>yada</p></>; #2
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//function Webpage() {
+//  return <section><header><h1>Hello</h1></header><p>yada, yada</p></section>
+//} #3
+
+//function Webpage() {
+//  const name = "Lisa Savoie";
+//  return <><section><header><h1>Hello {name}</h1></header><p>yada, yada</p><//section><Clock /></>
+//}
+//function Clock() {
+//  return <p>It's currently: {new Date().toLocaleTimeString("UK")}</p>
+//} #4
+
+//function Webpage() {
+//  const name = "Lisa Savoie"
+//  const date = new Date();
+//  return <><section><header><h1>Hello {name}</h1></header><p>yada, yada</p></section><Clock time={date}/></>
+//}
+//function Clock(props) {
+//  return<p>It's currently: {props.time.toLocaleTimeString("UK")}</p>
+//} #5
+
+//function Webpage() {
+//  const name = "Lisa Savoie"
+//  return <><section><header><h1>Hello {name}</h1></header><p>yada, yada</p></section><Clock /></>
+//}
+//function Clock() {
+//  const [time, setTime] = useState(new Date().toLocaleTimeString("UK"));
+//  setInterval(() => setTime(new Date().toLocaleTimeString("UK")), 1000);
+//  return<p>It's currently: {time}</p>
+//} //#6
+
+ReactDOM.render(< App />, document.getElementById("root")); //#7 Components & multiple pages
+//ReactDOM.render(< Webpage />, document.getElementById("root")); // #3,4,5,6
+//#3 function, #4 function with a variable, #5 props, #6 State
+//ReactDOM.render(element, document.getElementById("root")); #2 //Use a var
+//ReactDOM.render("Hello world", document.getElementById("root")); only needs reactDOM, react allow JSX #1 //Just printing to the page
